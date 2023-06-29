7a96c095ed6288e4dcade01b4476a752b5fa65b6
Alefantozzi86 <alefantozzi86@gmail.com>
2023-06-29 16:22:24 +0000

    Añadir contacto Listo

--- src/js/views/addcontacto.js
+++ src/js/views/addcontacto.js
@@ -3,71 +3,87 @@ import { Link, useNavigate } from "react-router-dom";
 import "../../styles/demo.css";
 
 export const AddContacto = () => {
-  const navigate = useNavigate()
-  const {data, setData } = useState({
-		full_name: "",
-		email: "",
-		agenda_slug: "Alefantozzi",
-		address: "",
-		phone: ""
-	});
- 
-  const handleSubmit = (event)=>{
+  const navigate= useNavigate();
+   const [data, setData] = useState({
+    "full_name": "",
+    "email": "",
+    "agenda_slug": "Alefantozzi",
+    "address":"",
+    "phone":""
+  });
+
+  const handleChange=(e)=>{
+    setData({...data,[e.target.name]:e.target.value});
+
+  }
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const text={
-
+    const config = {
       method: "POST",
-			body: JSON.stringify(data),
-			headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
-      
-      fetch ("https://assets.breatheco.de/apis/fake/contact/", text) 
-        .then ((resp) => resp.json())
-        .catch (error => console.error("error:", error))
-        .then ((response) => navigate("/"))
-      }
-  
-
+    fetch('https://assets.breatheco.de/apis/fake/contact/', config)
+      .then((resp) => resp.json())
+      .then((response) => navigate("/"))
+      .then (response =>console.log("contacto agregado"))
+      .then (response=> alert("El contacto se ha agregado"))
+      .catch((error) => console.error("error:", error))
+  };
   return (
     <div className="container">
+      <h1>Add Contact</h1>
       <form onSubmit={handleSubmit}>
-        
-        <div className="mb-3">
-          <label for="fullName" className="form-label">
-            Full Name
-          </label>
-          <input type="text" className="form-control" id="full_name" placeholder="Full Name" onChange={(event) => {
-							setData({...data, full_name: event.target.value})
-						}}/>
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="full-name"
+            placeholder="Enter Full Name"
+            name="full_name"
+            onChange={handleChange}
+          />
         </div>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email
-          </label>
-          <input type="text" className="form-control" id="email" placeholder="Enter Email" onChange={(event) => {
-							setData({...data, email: event.target.value})
-						}}/>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter Email"
+            name="email"
+            onChange={handleChange}
+          />
         </div>
-        <div className="mb-3">
-          <label for="exampleInput" className="form-label" id="phone" >
-            Phone
-          </label>
-          <input type="phone" className="form-control"id="phone" placeholder="Enter Phone" onChange={(event) =>{
-          setData ({...data, phone: event.target.value})
-          }} />
+        <div className="form-group">
+          <label htmlFor="phone" className="form-label">Phone</label>
+          <input
+            type="phone"
+            className="form-control"
+            id="phone"
+            placeholder="Enter Phone"
+            name="phone"
+            onChange={handleChange}
+          />
+          </div>
+          <div className="form-group">
+          <label for="address" className="form-label">Address</label>
+          <input
+            type="text"
+            className="form-control"
+            id="address"
+            placeholder="Enter Address"
+            name="address"
+            onChange={handleChange}
+          />
         </div>
-		<label for="fullName" className="form-label">
-          Address
-          </label>
-          <input type="fullname" className="form-control" id="address" placeholder="Enter Address" onChange={(event)=>{
-            setData({...data, address: event.target.value})
-          }}/>
-          
         <button type="submit" className="container-fluid btn btn-primary m-4">
        Save
         </button>
       </form>
-
       <Link to="/">
         <button className="btn btn-primary">Back to contact</button>
       </Link>
